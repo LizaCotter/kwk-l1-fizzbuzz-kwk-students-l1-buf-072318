568189a5a2f327b56f7281b7304b6eda9dd7bac0
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-25 14:35:35 +0000

    automatically backed up by learn

---
+++ bucket_list.rb
@@ -0,0 +1,2 @@
+bucket_list=["Become fluent in Spanish", "Bike trip across the US w/ Mary", "Skydive!"]
+puts bucket_list
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,25 @@
+#FizzBuzz Part 1
+# def fizzbuzz(number)
+#   if number % 15 == 0 
+#     "FizzBuzz"
+#   elsif number % 3 == 0 
+#     "Fizz"
+#   elsif number % 5 == 0 
+#     "Buzz"
+#   end 
+# end
+
+
 def fizzbuzz(number)
   if number % 15 == 0 
     "FizzBuzz"
   elsif number % 3 == 0 
     "Fizz"
   elsif number % 5 == 0 
-    "Buzz"
+     "Buzz"
   end 
 end
+  puts "What number would you like to FizzBuzzify?"
+  number = get.chomp
 
-puts fizzbuzz(4)
+fizzbuzz(number)
